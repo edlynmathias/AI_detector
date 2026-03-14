3f8e1b43213637fe1d48b5852e2f7fc6f08feb12
edlynmathias <edlyn13zzz@gmail.com>
2026-03-14 15:44:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+
+# ------------------------
+# Load model
+# ------------------------
+model = load_model("my_model.h5")
+
+IMG_SIZE = (32,32)
+
+# ------------------------
+# Preprocess image
+# ------------------------
+def preprocess(img):
+    img = img.resize(IMG_SIZE)
+    img = np.array(img)/255.0
+    img = np.expand_dims(img, axis=0)
+    return img
+
+# ------------------------
+# Streamlit UI
+# ------------------------
+st.title("AI vs Real Image Detector")
+
+uploaded_file = st.file_uploader("Upload an Image", type=["jpg","png","jpeg"])
+
+if uploaded_file is not None:
+
+    img = Image.open(uploaded_file)
+
+    st.image(img, caption="Uploaded Image", use_column_width=True)
+
+    img_input = preprocess(img)
+
+    prediction = model.predict(img_input)[0][0]
+
+    if prediction > 0.5:
+        label = "REAL IMAGE"
+    else:
+        label = "AI GENERATED"
+
+    st.subheader(f"Prediction: {label}")
+    st.write(f"Confidence Score: {prediction:.4f}")
